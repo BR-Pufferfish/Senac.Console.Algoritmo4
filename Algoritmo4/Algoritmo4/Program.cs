b530c42400b532777022e4d4ff7fc8d5579cb515
Marcelo Ballin <puffersenac@gmail.com>
2025-04-01 21:04:27 -0300

    Código escrito e comentado.

--- Algoritmo4/Algoritmo4/Program.cs
+++ Algoritmo4/Algoritmo4/Program.cs
@@ -1,1 +1,18 @@
-﻿Console.WriteLine("Hello, World!");
+﻿//Biblioteca do CultureInfo.InvariantCulture
+using System.Globalization;
+
+//Variáveis
+double largura, comprimento, area, valorMetro, valorTerreno;
+
+//Entrada de dados.
+largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+comprimento = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+valorMetro = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+//Calculo de dados.
+area = largura * comprimento;
+valorTerreno = area * valorMetro;
+
+//Retorno visual.
+Console.WriteLine("AREA - " + area.ToString("F2", CultureInfo.InvariantCulture));
+Console.WriteLine("PRECO = "+valorTerreno.ToString("F2", CultureInfo.InvariantCulture));
